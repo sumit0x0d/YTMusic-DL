7177d3b09202971dd857b4e3f4927defc0b89aa9
sumit0x0d <86312382+sumit0x0d@users.noreply.github.com>
2021-12-02 01:21:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
 """
 YouTube Music Downloader
-GOAL:
-    Downloading highest quality music from https://music.youtube.com
-REQUIREMENTS:
-    python-requests
-    ffmpeg
-    Netscape HTTP Cookie File For itag 141
-USAGE:
-    python __main__.py <VIDEO_ID> <ITAG>
-CREDITS:
-    youtube-dl https://github.com/ytdl-org/youtube-dl
-    pytube https://github.com/pytube/pytube
-    innertube https://github.com/tombulled/innertube
-    ytmusicapi https://github.com/sigma67/ytmusicapi
 """
 
 import hashlib
